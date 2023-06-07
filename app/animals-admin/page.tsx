7a07f3db829ef52836ed2ecf465ff8ba2252615d
David Ihl <daih1985@gmail.com>
2023-06-07 10:26:02 +0200

    Add force dynamic to animals-admin to prevent deployment fail

--- app/animals-admin/page.tsx
+++ app/animals-admin/page.tsx
@@ -1,6 +1,9 @@
 import { getAnimals } from '../../database/animals';
 import AnimalsForm from './AnimalsForm';
 
+// Use 'force-dynamic' on pages using database queries in the server component to prevent your deployment to fail.
+export const dynamic = 'force-dynamic';
+
 export default async function AnimalsAdminPage() {
   const animals = await getAnimals();
   return <AnimalsForm animals={animals} />;

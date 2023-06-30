5678e4b06180469f0ca87a24cb0ffdac810b5ffd
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-06-30 16:33:54 +0200

    Update eslint

--- database/connect.ts
+++ database/connect.ts
@@ -1,50 +1,28 @@
-import 'server-only';
-import { config } from 'dotenv-safe';
-import { headers } from 'next/headers';
-import postgres, { Sql } from 'postgres';
+import postgres from 'postgres';
+import { setEnvironmentVariables } from '../ley.config.mjs';
 
-if (!process.env.FLY_IO) config();
+// This loads all environment variables from a .env file
+// for all code after this line
+setEnvironmentVariables();
 
+// Type needed for the connection function below
 declare module globalThis {
-  let postgresSqlClient: Sql;
+  let postgresSqlClient: ReturnType<typeof postgres> | undefined;
 }
 
 // Connect only once to the database
 // https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
 function connectOneTimeToDatabase() {
-  if (!('postgresSqlClient' in globalThis)) {
+  if (!globalThis.postgresSqlClient) {
     globalThis.postgresSqlClient = postgres({
-      host: process.env.POSTGRES_HOST || process.env.PG_HOST,
-      username: process.env.POSTGRES_USER || process.env.PGUSERNAME,
-      password: process.env.POSTGRES_PASSWORD || process.env.PGPASSWORD,
-      database: process.env.POSTGRES_DATABASE || process.env.PGDATABASE,
-      ssl: !!process.env.POSTGRES_URL,
+      ssl: Boolean(process.env.POSTGRES_URL),
       transform: {
         ...postgres.camel,
         undefined: null,
       },
     });
   }
-
-  // Workaround to force Next.js Dynamic Rendering:
-  //
-  // Wrap sql`` tagged template function to call `headers()` from
-  // next/headers before each database query. `headers()` is a
-  // Next.js Dynamic Function, which causes the page to use
-  // Dynamic Rendering.
-  //
-  // https://nextjs.org/docs/app/building-your-application/rendering/static-and-dynamic-rendering
-  //
-  // Ideally there would something built into Next.js for this,
-  // which has been requested here:
-  //
-  // https://github.com/vercel/next.js/discussions/50695
-  return ((
-    ...sqlParameters: Parameters<typeof globalThis.postgresSqlClient>
-  ) => {
-    headers();
-    return globalThis.postgresSqlClient(...sqlParameters);
-  }) as typeof globalThis.postgresSqlClient;
+  return globalThis.postgresSqlClient;
 }
 
 // Connect to PostgreSQL

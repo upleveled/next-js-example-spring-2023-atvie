c0b4cbba6c8134b6b56667317d1e163f32e1b006
Lukas Prochazka <prochazka.lu@gmail.com>
2023-08-31 12:54:35 +0200

    Update playwright config and yaml

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,34 +1,22 @@
-import { devices, PlaywrightTestConfig } from '@playwright/test';
+import { PlaywrightTestConfig } from '@playwright/test';
 
-// config file reference - https://playwright.dev/docs/test-configuration
+// Config file docs: https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: 'pnpm start',
+    command: './node_modules/.bin/next start',
     port: 3000,
-    reuseExistingServer: !process.env.CI,
+    stdout: 'pipe',
   },
-  testIgnore: '**/util/__tests__/**',
-  retries: process.env.CI ? 2 : 0,
+  testMatch: '**/playwright/**',
   reporter: process.env.CI
-    ? 'list'
+    ? 'github'
     : [['html', { outputFolder: 'playwright/report/' }]],
   outputDir: 'playwright/test-results/',
-
   use: {
-    trace: 'on-first-retry',
+    testIdAttribute: 'data-test-id',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    testIdAttribute: 'data-test-id',
   },
-
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
-  ],
 };
 
 export default config;
